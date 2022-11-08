3ce35779fdf916e113cae96cbebc9e5f3227cdc8
KirillDmit <kirilldmitriev20015@gmail.com>
2022-11-08 21:56:32 +0500

    init

--- src/main/java/ru/naumen/course/repositories/LibraryRepository.java
+++ src/main/java/ru/naumen/course/repositories/LibraryRepository.java
@@ -1,12 +1,9 @@
 package ru.naumen.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.naumen.course.models.City;
 import ru.naumen.course.models.Library;
-import ru.naumen.course.models.Person;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface LibraryRepository extends JpaRepository<Library, Integer> {
     List<Library> findByOwnerId(Integer ownerId);
