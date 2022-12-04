e1d7b04061b4985bbfbff11dca004f6d2f3889ab
Denis <88247988+BooperBo@users.noreply.github.com>
2022-12-04 17:58:39 +0500

    Add files via upload

---
+++ RestoreProjectLibrary/src/main/java/ru/naumen/ProjectLibrary/controllers/LibraryControllers.java
@@ -0,0 +1,42 @@
+package ru.naumen.ProjectLibrary.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.naumen.ProjectLibrary.services.LibraryService;
+
+@Controller
+@RequestMapping("/libraries")
+public class LibraryControllers {
+
+    private final LibraryService libraryService;
+
+    @Autowired
+    public LibraryControllers(LibraryService libraryService) {
+        this.libraryService = libraryService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("library", libraryService.findAll());
+        return "libraries/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("library", libraryService.findOne(id));
+        model.addAttribute("library", libraryService.getLibraryById(id));
+
+        return "libraries/show";
+    }
+
+    @GetMapping("/owner/{id}")
+    public String showByOwnerId(@PathVariable("id") int id, Model model) {
+        model.addAttribute("library", libraryService.findByOwnerId(id));
+        return "libraries/show";
+    }
+}
+
